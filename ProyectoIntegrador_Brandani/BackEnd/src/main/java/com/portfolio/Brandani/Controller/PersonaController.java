c1f3ff60fac81ebe06ed06e14b65f3fc1711293a
JavierBrandani <100771211+JavierBrandani@users.noreply.github.com>
2022-08-24 08:19:18 -0300

    back

--- ProyectoIntegrador_Brandani/BackEnd/src/main/java/com/portfolio/Brandani/Controller/PersonaController.java
+++ ProyectoIntegrador_Brandani/BackEnd/src/main/java/com/portfolio/Brandani/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontend-8d148.web.app")
+@CrossOrigin(origins = "https://frontend-8d148.web.app/")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
