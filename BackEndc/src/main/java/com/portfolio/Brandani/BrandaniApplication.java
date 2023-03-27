92a8270cba7215adba121057988b2ed488a2fc02
JavierBrandani <100771211+JavierBrandani@users.noreply.github.com>
2023-03-27 11:17:23 -0300

    backendc

---
+++ BackEndc/src/main/java/com/portfolio/Brandani/BrandaniApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Brandani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandaniApplication.class, args);
+	}
+
+}
